396cad6299882861ddfed25960860b850139cc66
lizepeng <lizepeng@daling.com>
2020-06-03 11:57:12 +0800

    feat:flutterbase改为flutter_base

--- test/flutter_base_test.dart
+++ test/flutter_base_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_base/flutter_base.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
