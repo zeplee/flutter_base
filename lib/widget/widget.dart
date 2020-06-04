88da68b6cfb9d3f4be8b72d41af029d282554ee6
lizepeng <lizepeng@daling.com>
2020-06-04 11:53:02 +0800

    feat:加入svg支持

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -2,6 +2,7 @@ library widget;
 
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:flutter_svg/flutter_svg.dart';
+
 export 'common_dlg.dart';
 export 'common_drag.dart';
 export 'common_float.dart';
