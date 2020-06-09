829cdce563fc4468e33d92c1432498db291850d6
lizepeng <lizepeng@daling.com>
2020-06-09 16:19:13 +0800

    feat:库升级

--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -1,9 +1,9 @@
 library helper;
 
 export 'package:flustars/flustars.dart';
+export 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 export 'package:provider/provider.dart';
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 export 'bus_helper.dart';
 export 'net_helper.dart';
