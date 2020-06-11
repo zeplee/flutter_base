7969d76e5ca6f6d2a8b9d13b2b73ba99bc13ec66
lizepeng <lizepeng@daling.com>
2020-06-11 10:16:34 +0800

    feat:库加入example

--- example/android/app/src/main/kotlin/com/flutter_native_base_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/flutter_native_base_example/MainActivity.kt
@@ -3,4 +3,22 @@ package com.flutter_base_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    //    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        when {
+//            //19->21
+//            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP -> {
+//                window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+//                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+////                window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+////                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+//            }
+//            //21->max
+//            Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP -> window.statusBarColor = Color.TRANSPARENT
+//        }
+//    }
+//
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
 }
